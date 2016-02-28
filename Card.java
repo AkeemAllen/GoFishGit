b5895b6cc60b3ba25e1c2c360dae514f2396874d
CheapUniform <avstye@gmail.com>
2016-02-27 22:42:21 -0700

    changes were made.

--- Card.java
+++ Card.java
@@ -1,18 +1,7 @@
 public class Card {
-	//why do we need these? they are never used ... 
-	private static final int SPADE = 0;
-	private static final int DIAMOND = 1;
-	private static final int HEART = 2;
-	private static final int CLUB = 3;
-	//why do we need these? they are never used ... 
-	private static final int ACE = 1;
-	private static final int JACK = 11;
-	private static final int QUEEN = 12;
-	private static final int KING = 13;
 
 	private int suit;
 	private int value;
-	
 
 	//Creates a card
 	public Card(int value, int suit) 
@@ -46,18 +35,18 @@ public class Card {
 	{
 		switch (this.value) 
 		{
-			case 1: return "Ace";
-			case 2: return "Two";
-			case 3: return "Three";
-			case 4: return "Four";
-			case 5: return "Five";
-			case 6: return "Six";
-			case 7: return "Seven";
-			case 8: return "Eight";
-			case 9: return "Nine";
-			case 10: return "Ten";
-			case 11: return "Jack";
-			case 12: return "Queen";
+			case 0: return "Ace";
+			case 1: return "Two";
+			case 2: return "Three";
+			case 3: return "Four";
+			case 4: return "Five";
+			case 5: return "Six";
+			case 6: return "Seven";
+			case 7: return "Eight";
+			case 8: return "Nine";
+			case 9: return "Ten";
+			case 10: return "Jack";
+			case 11: return "Queen";
 			default: return "King";
 		}
 	}

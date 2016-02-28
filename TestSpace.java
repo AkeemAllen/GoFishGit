77c03cb93928fb49aaa9f5dbcb96bb357c8e3895
Andrew Fiorentino <fiorentinoandrew@gmail.com>
2016-02-28 00:23:39 -0800

    Update TestSpace.java
    Just updated the test driver class to demonstrate the changes I made

--- TestSpace.java
+++ TestSpace.java
@@ -1,11 +1,18 @@
 public class TestSpace {
 
 	public static void main(String[] args) {
-	
-	Deck deck1 = new Deck();
 
-	deck1.printDeck();
+		Deck deck1 = new Deck();
 
+		deck1.printDeck();
+		Player user = new Player();
+		Computer computer = new Computer();
+		System.out.println(user);
+		System.out.println(computer);
+		deck1.dealCard(user);
+		deck1.printDeck();
 
+		deck1.shuffle();
+		deck1.printDeck();
 	}
 }

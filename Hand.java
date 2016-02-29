c137923decbd384ed28f8335f75fad0a59d0768d
Andrew Fiorentino <fiorentinoandrew@gmail.com>
2016-02-28 19:15:58 -0800

    Update Hand.java

--- Hand.java
+++ Hand.java
@@ -1,27 +1,37 @@
 import java.util.*;
 
 public class Hand {
-	public ArrayList<Card> Hand; 
-	
-	public	Hand(){
-		Hand = new ArrayList<Card>(); 
+	public ArrayList<Card> Hand;
+
+	public Hand() {
+		Hand = new ArrayList<Card>();
 	}
-	public void drawCard(Deck deckObject){
+
+	public void drawCard(Deck deckObject) {
 		Card tempCard = deckObject.getTopCard();
-		Hand.add(tempCard);
+		this.Hand.add(tempCard);
 		deckObject.removeTopCard();
 	}
-	public void dealHand(Deck deckObject){
-		for (int i = 0; i < 7; i++){
+
+	public void dealHand(Deck deckObject) {
+		for (int i = 0; i < 7; i++) {
 			Card tempCard = deckObject.getTopCard();
-			Hand.add(tempCard);
+			this.Hand.add(tempCard);
 			deckObject.removeTopCard();
 		}
 	}
-	protected void printHand()
-	{
-		for (int i = 0; i < Hand.size(); i++) 
-		{
+
+	public void takeCard(Hand playerHand, Card cardObject) {
+		this.Hand.add(cardObject);
+		playerHand.removeCard(cardObject);
+	}
+
+	public void removeCard(Card cardObject) {
+		this.Hand.remove(cardObject);
+	}
+
+	protected void printHand() {
+		for (int i = 0; i < Hand.size(); i++) {
 			System.out.println(this.Hand.get(i).valueToString() + " of " + this.Hand.get(i).suitToString());
 		}
 	}

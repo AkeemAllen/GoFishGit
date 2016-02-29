9c1d4585d9b51eaccd306b38f9fe4799d7f25ad5
Andrew Fiorentino <fiorentinoandrew@gmail.com>
2016-02-28 21:24:33 -0800

    Update Hand.java

--- Hand.java
+++ Hand.java
@@ -2,9 +2,10 @@ import java.util.*;
 
 public class Hand {
 	public ArrayList<Card> Hand;
-
+	public ArrayList<Card> Discard;
 	public Hand() {
 		Hand = new ArrayList<Card>();
+		Discard = new ArrayList<Card>();
 	}
 
 	public void drawCard(Deck deckObject) {
@@ -30,7 +31,8 @@ public class Hand {
 		playerHand.removeCard(cardObject);
 	}
 
-	public void removeCard(Card cardObject) {
+	public void removeCard(cardIndex) {
+		Discard.add(Hand.get(cardIndex));
 		this.Hand.remove(cardObject);
 	}
 
@@ -39,4 +41,27 @@ public class Hand {
 			System.out.println(this.Hand.get(i).valueToString() + " of " + this.Hand.get(i).suitToString());
 		}
 	}
+
+	public int testSelfCard (String testedCard){
+		boolean testSelf = false;
+		for (int i = 0; i < Hand.size(); i ++){
+			if (testedCard == Hand.get(i).valueToString()){
+				testSelf = true;
+				int selfCardIndex = Hand.indexOf(Hand.get(i));
+				return selfCardIndex;
+			}
+		}
+	}
+	public int requestCard(String testedCard) {
+		boolean cardInHand = false;
+		for (int i = 0; i < Hand.size(); i++) {
+			if (testedCard == Hand.get(i).valueToString()) {
+				cardInHand = true;
+				int cardIndex = Hand.indexOf(Hand.get(i));
+				
+				
+			}
+		}
+	}
+
 }

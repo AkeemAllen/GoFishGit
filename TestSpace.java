8ff836b61cf6be4cf170582bb34cbb64d04ab590
Jesse Sargent <mountaneer12@gmail.com>
2016-02-28 19:32:40 -0800

    Changed hand to getHand()

--- TestSpace.java
+++ TestSpace.java
@@ -6,11 +6,11 @@ public class TestSpace {
 		deck.shuffle();
 		deck.printDeck();
 		Player player1 = new Player("Alpha");
-		player1.hand.drawCard(deck);
+		player1.getHand().drawCard(deck);
 		System.out.println("\n\n\nPrinting delt deck: \n");
 		deck.printDeck();
 		System.out.println("\n\n\nPrint delt hand: \n");
-		player1.hand.printHand();
+		player1.getHand().printHand();
 
 	
 	for (int i = 0; i < deck.getSize(); i++) {

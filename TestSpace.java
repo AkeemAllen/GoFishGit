b3ea0e322d877e72411c361f50d7d7f352168c7b
Andrew Fiorentino <fiorentinoandrew@gmail.com>
2016-02-28 19:21:10 -0800

    Update TestSpace.java

--- TestSpace.java
+++ TestSpace.java
@@ -1,10 +1,16 @@
 public class TestSpace {
 
 	public static void main(String[] args) {
-	
-	Deck deck = new Deck();
-	deck.shuffle();
-	deck.printDeck();
+
+		Deck deck = new Deck();
+		deck.shuffle();
+		deck.printDeck();
+		Player player1 = new Player("Alpha");
+		player1.hand.drawCard(deck);
+		System.out.println("\n\n\nPrinting delt deck: \n");
+		deck.printDeck();
+		System.out.println("\n\n\nPrint delt hand: \n");
+		player1.hand.printHand();
 
 	}
 }

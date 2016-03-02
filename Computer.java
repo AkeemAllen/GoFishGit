5603ac6e357724042345b8c48153b1db674e7679
Andrew Fiorentino <fiorentinoandrew@gmail.com>
2016-03-01 19:22:28 -0800

    Update Computer.java
    final version

--- Computer.java
+++ Computer.java
@@ -1,20 +1,6 @@
 
-public class Computer extends Player{
+public class Computer extends Player {
 	public Computer(String name) {
 		super(name);
-		// TODO Auto-generated constructor stub
 	}
-
-	void requestCard(){
-		/*I'm pretty sure this is the only method that needs to be added
-		 * or overriden for this class. I think that the best way to set 
-		 * this up is to use a random number generator to select a random
-		 * card from the user's hand. If we REALLY wanted to, we could use
-		 * some basic AI and have the computer notice when a player draws a
-		 * card the the computer has in it's hand, but I really just want to 
-		 * get this project done with. Like I would be so happy if the words 
-		 * "personal software process" were never mentioned again.
-		 */
-	}
-
 }
